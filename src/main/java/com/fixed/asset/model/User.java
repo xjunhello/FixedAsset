ede810482c4f247aa5511346802e5e9073c135d5
张信军 <xinjun.zhang@mtime.com>
2018-02-08 14:19:45 +0800

    add spring security

--- src/main/java/com/fixed/asset/model/User.java
+++ src/main/java/com/fixed/asset/model/User.java
@@ -1,10 +1,19 @@
 package com.fixed.asset.model;
 
+import java.util.List;
 
 public class User {
 	private Integer id;
 	private String name;
 	private String password;
+	private List<Role> roles;
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
 
 	public Integer getId() {
 		return id;

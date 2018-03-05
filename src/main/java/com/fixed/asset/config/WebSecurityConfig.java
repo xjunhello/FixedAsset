4cff9b2777dc67d30bba28793cd646009ba1c7ed
张信军 <xinjun.zhang@mtime.com>
2018-03-05 18:24:09 +0800

    xxx

--- src/main/java/com/fixed/asset/config/WebSecurityConfig.java
+++ src/main/java/com/fixed/asset/config/WebSecurityConfig.java
@@ -26,8 +26,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated() // 登录后可以访问
-                .antMatchers("/static/*").permitAll() //静态资源开放权限
+                //.anyRequest().authenticated() // 登录后可以访问
+                .antMatchers("/static/.*").permitAll() //静态资源开放权限
                 .and()
                 .formLogin()
                 .loginPage("/login")

e2d2c57219422f566fcffa57f9e46aecd7700423
张信军 <xinjun.zhang@mtime.com>
2018-03-09 17:57:55 +0800

    前端架构

--- src/main/java/com/fixed/asset/web/UserController.java
+++ src/main/java/com/fixed/asset/web/UserController.java
@@ -1,5 +1,21 @@
 package com.fixed.asset.web;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fixed.asset.model.Msg;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	 	@RequestMapping("/get")
+	    public String get(Model model){
+	        return "user_list";
+	    }
+	 	
+	 	@RequestMapping("/add")
+	    public String add(Model model){
+	        return "user_modify";
+	    }
 }
